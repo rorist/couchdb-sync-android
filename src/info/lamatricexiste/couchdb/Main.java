e9695d738f7368fa18acceca551e5b0a9e7333f4
Aubort Jean-Baptiste <rorist@gmail.com>
2011-07-11 15:36:20 +0200

    couchdb base app

--- src/info/lamatricexiste/couchdb/Main.java
+++ src/info/lamatricexiste/couchdb/Main.java
@@ -5,11 +5,26 @@ import android.os.Bundle;
 
 public class Main extends Activity
 {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        String release = "release-0.1";
+        ServiceConnection couchServiceConnection = CouchDB.getService(getBaseContext(), null, release, mCallback);
     }
+
+    private final ICouchClient mCallback = new ICouchClient.Stub() {
+        @Override
+        public void couchStarted(String host, int port) {}
+
+        @Override
+        public void installing(int completed, int total) {}
+
+        @Override
+        public void downloading(int completed, int total) {}
+
+        @Override
+        public void exit(String error) {}
+    };
 }
