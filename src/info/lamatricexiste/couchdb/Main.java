7fa90f6d1407c68618be11d642455f2730719000
Aubort Jean-Baptiste <rorist@gmail.com>
2011-07-11 15:43:52 +0200

    Working project

--- src/info/lamatricexiste/couchdb/Main.java
+++ src/info/lamatricexiste/couchdb/Main.java
@@ -1,30 +1,36 @@
 package info.lamatricexiste.couchdb;
 
 import android.app.Activity;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 
-public class Main extends Activity
-{
+import com.couchbase.libcouch.CouchDB;
+import com.couchbase.libcouch.ICouchClient;
+
+public class Main extends Activity {
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         String release = "release-0.1";
-        ServiceConnection couchServiceConnection = CouchDB.getService(getBaseContext(), null, release, mCallback);
+        CouchDB.getService(getBaseContext(), null, release, mCallback);
     }
 
     private final ICouchClient mCallback = new ICouchClient.Stub() {
         @Override
-        public void couchStarted(String host, int port) {}
+        public void couchStarted(String host, int port) {
+        }
 
         @Override
-        public void installing(int completed, int total) {}
+        public void installing(int completed, int total) {
+        }
+
+        // @Override
+        // public void downloading(int completed, int total) {
+        // }
 
         @Override
-        public void downloading(int completed, int total) {}
-
-        @Override
-        public void exit(String error) {}
+        public void exit(String error) {
+        }
     };
 }
