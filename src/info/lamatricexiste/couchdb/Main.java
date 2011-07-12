d144861a1aa34c360592fe9e68797c6eb4e84784
Aubort Jean-Baptiste <rorist@gmail.com>
2011-07-12 16:51:25 +0200

    Try to run CouchDB service of the market

--- src/info/lamatricexiste/couchdb/Main.java
+++ src/info/lamatricexiste/couchdb/Main.java
@@ -1,16 +1,16 @@
 package info.lamatricexiste.couchdb;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.couchbase.libcouch.CouchDB;
-import com.couchbase.libcouch.ICouchClient;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.widget.Toast;
 
 public class Main extends Activity {
-
-    private ServiceConnection couchServiceConnection;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,24 +35,36 @@ public class Main extends Activity {
     }
 
     private void startCouch() {
-        couchServiceConnection = CouchDB.getService(getBaseContext(), null, "release-0.1",
-                mCallback);
+        bindService(new Intent("com.arandomurl.couchapp.ICouchService"), couchServiceConnection,
+                Context.BIND_AUTO_CREATE);
     }
 
-    private final ICouchClient mCallback = new ICouchClient.Stub() {
+    private ServiceConnection couchServiceConnection = new ServiceConnection() {
+
         @Override
-        public void couchStarted(String host, int port) {
-            Log.e("COUCH", "host=" + host + ", port=" + port);
+        public void onServiceConnected(ComponentName name, IBinder service) {
         }
 
         @Override
-        public void installing(int completed, int total) {
-            Log.e("COUCH", "completed=" + completed + ", total=" + total);
+        public void onServiceDisconnected(ComponentName name) {
+        }
+
+    };
+
+    private ICouchClient couchClient = new ICouchClient.Stub() {
+
+        @Override
+        public void couchStarted(String host, int port) throws RemoteException {
+            Toast.makeText(Main.this, "host=" + host + ", port=" + port, Toast.LENGTH_LONG);
         }
 
         @Override
-        public void exit(String error) {
-            Log.e("COUCH", "exit=" + error);
+        public void exit(String error) throws RemoteException {
         }
+
+        @Override
+        public void installing(int completed, int total) throws RemoteException {
+        }
+
     };
 }
