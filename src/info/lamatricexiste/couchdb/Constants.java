76001b8f99588a82b4b58110fd9b4dc04922d210
Aubort Jean-Baptiste <rorist@gmail.com>
2011-07-15 16:41:16 +0200

    Change package name to budiez

--- src/info/lamatricexiste/couchdb/Constants.java
+++
@@ -1,8 +0,0 @@
-package info.lamatricexiste.couchdb;
-
-public class Constants {
-
-    public static final String ACCOUNT_TYPE = "info.lamatricexiste.couchdb";
-    public static final String AUTHTOKEN_TYPE = "info.lamatricexiste.couchdb";
-    
-}
