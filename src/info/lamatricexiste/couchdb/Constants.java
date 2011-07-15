c50b7beedca43fa47d383d9e7396ec55b802ef74
Aubort Jean-Baptiste <rorist@gmail.com>
2011-07-15 16:31:54 +0200

    Base account managment for contact synchronisation

---
+++ src/info/lamatricexiste/couchdb/Constants.java
@@ -0,0 +1,8 @@
+package info.lamatricexiste.couchdb;
+
+public class Constants {
+
+    public static final String ACCOUNT_TYPE = "info.lamatricexiste.couchdb";
+    public static final String AUTHTOKEN_TYPE = "info.lamatricexiste.couchdb";
+    
+}
