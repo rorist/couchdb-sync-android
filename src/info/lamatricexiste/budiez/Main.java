127cc67b2e0a46d97faa93d430aaede3edbdedbe
Aubort Jean-Baptiste <aubort.jeanbaptiste@gmail.com>
2011-07-18 17:33:44 +0200

    Get contacts info

--- src/info/lamatricexiste/budiez/Main.java
+++ src/info/lamatricexiste/budiez/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.ref.PhantomReference;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,10 +14,13 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.provider.ContactsContract.Data;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -29,6 +33,7 @@ import com.couchbase.libcouch.ICouchService;
 public class Main extends Activity {
 
     private final static String ACTION = "com.couchone.libcouch.ICouchService";
+    private static final String TAG = "Main";
     private String mHost;
     private int mPort;
 
@@ -40,6 +45,24 @@ public class Main extends Activity {
         startCouch();
 
         // Buttons
+        findViewById(R.id.btn_contacts).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Query RAW contact data
+                Cursor c = getContentResolver().query(Data.CONTENT_URI,
+                        new String[] { Data._ID, Data.CONTACT_ID, Data.MIMETYPE, Data.DATA1 },
+                        null, null, null);
+                // Show to output
+                while (c.moveToNext()) {
+                    ((TextView) findViewById(R.id.output)).append(c.getString(c
+                            .getColumnIndex(Data.CONTACT_ID))
+                            + ": "
+                            + c.getString(c.getColumnIndex(Data.MIMETYPE))
+                            + ", "
+                            + c.getString(c.getColumnIndex(Data.DATA1)) + "\n");
+                }
+            }
+        });
         findViewById(R.id.btn_rep1).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
